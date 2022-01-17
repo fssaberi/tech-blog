389cc12b7f1cf141cb65bf37310c5ceba3912148
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-17 15:27:59 -0800

    update homepage-routes, add comments model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     console.log(req.session);
