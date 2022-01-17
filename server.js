6052a24cf951ef61e2f6d073f1bbdcefc010a525
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-17 15:22:22 -0800

    update helpers.js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
 const routes = require('./controllers');
-// const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
-
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -23,14 +22,12 @@ const aSession = {
 };
 
 app.use(session(aSession));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // connect to database and to server
 sequelize.sync({ force: false }).then(() => {
