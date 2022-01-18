79ca19bbcc67100299cf0dd5637103c9db88908a
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-17 17:28:26 -0800

    update post-info and comments handlebars files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -23,5 +23,3 @@ async function loginForm(event) {
   }
   
 document.querySelector('.login-form').addEventListener('submit', loginForm);
-
-
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const session = require('express-session');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'something secret',
-    cookie: {
-        expires: 20 * 50 * 1000
-    },
-    resave: true,
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
 };
 
-app.use(session(sess));
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
+
 app.use(routes);
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// connect to database and to server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Listening now!'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
