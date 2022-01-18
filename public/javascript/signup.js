f9a8a8811ee371d47c02e9a2093cd2a54a030af1
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-17 18:07:47 -0800

    update signup.js to add dashboard

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -18,6 +18,7 @@ async function signupForm(event) {
       
       if (response.ok) {
         console.log('success');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
