b34700777b1afb290e18a9a313d466a2534c588d
Fatemeh Brizendine <ssaberi324@yahoo.com>
2022-01-17 17:45:33 -0800

    update dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,6 +4,7 @@ const { User, Post, Comment } = require('../models');
 const yesAuthorized = require('../utils/auth');
 
 router.get('/', yesAuthorized, (req, res) => {
+    console.log(req.session);
     Post.findAll({
         where: {
             user_id: req.session.user_id
@@ -32,6 +33,85 @@ router.get('/', yesAuthorized, (req, res) => {
         .then(dbPostInfo => {
             const posts = dbPostInfo.map(post => post.get({ plain: true }));
             res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/edit/:id', yesAuthorized, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostInfo => {
+            if (dbPostInfo) {
+                const post = dbPostInfo.get({ plain: true });
+
+                res.render('edit-post', {
+                    post,
+                    loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.get('/create/', yesAuthorized, (req, res) => {
+    Post.findAll({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostInfo => {
+            const posts = dbPostInfo.map(post => post.get({ plain: true }));
+            res.render('create-post', { posts, loggedIn: true });
         })
         .catch(err => {
             console.log(err);
